fb7cc6cdbe07437f62ecd98c1a369a0893e31ac0
anutatesl <tesl.anna03@mail.ru>
2023-07-13 18:43:25 +0300

    divided the transmission file into fragments

--- project/main.cpp
+++ project/main.cpp
@@ -1,15 +1,15 @@
 #include "COM_PORT_INIT.h"
 #include <stdio.h>
 #include <iostream>
+//#pragma comment(lib, "libname.lib")
 
 int main() {
 	Com_port port;
 	if (port.openPort("COM1", 9600)) {
-		std::cout << "yes\n";
-		port.writeData();
+		port.writeData("hello.txt");
 		//port.readData();
 		port.closePort();
 	} else {
-		std::cout << "no\n";
+		std::cout << "Opening error\n";
 	}
 }
