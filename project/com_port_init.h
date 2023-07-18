785d5ef4d0884dded8df23a24509732c0e4d1cea
sonix14 <roletskaya_sof@mail.ru>
2023-07-19 02:39:54 +0300

    expanded recieve function

--- project/com_port_init.h
+++ project/com_port_init.h
@@ -1,4 +1,4 @@
-﻿#ifndef COM_PORT_INIT_H
+﻿﻿#ifndef COM_PORT_INIT_H
 #define COM_PORT_INIT_H
 
 #include <windows.h>
@@ -13,7 +13,7 @@ struct Com_port {
     bool openPort(const std::string& port, int baudrate);
     void closePort();
 
-    virtual void writeData(const std::string& file);
+    virtual void writeData(const char str);  //const std::string& file
     virtual void readData(char* dst);
 
 private:
@@ -21,7 +21,6 @@ private:
     bool installPortTimeouts();
 
     wchar_t* convertToLPCTSTR(const std::string& str);
-    bool getAnswer();
 };
 
 #endif
